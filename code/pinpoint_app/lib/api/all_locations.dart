ee9d231a61156ea6e4558c5c4b77fd439f67be27
Sens van Aert <r0878915@student.thomasmore.be>
2023-11-29 14:06:05 +0100

    Working tracking / update to cloudflare

--- code/pinpoint_app/lib/api/all_locations.dart
+++ code/pinpoint_app/lib/api/all_locations.dart
@@ -31,20 +31,21 @@ class User {
   final double lat;
   final double lon;
   final String timeCreated;
+  final String? timeModified;
 
-  const User({
-    required this.name,
-    required this.lat,
-    required this.lon,
-    required this.timeCreated,
-  });
+  const User(
+      {required this.name,
+      required this.lat,
+      required this.lon,
+      required this.timeCreated,
+      this.timeModified});
 
   factory User.fromJson(Map<String, dynamic> json) {
     return User(
-      name: json['name'],
-      lat: json['lat'],
-      lon: json['lon'],
-      timeCreated: json['timeCreated'],
-    );
+        name: json['name'],
+        lat: json['lat'],
+        lon: json['lon'],
+        timeCreated: json['timeCreated'],
+        timeModified: json['timeModified']);
   }
 }
--- code/pinpoint_app/lib/screens/pinpoint/location.dart
+++ code/pinpoint_app/lib/screens/pinpoint/location.dart
@@ -1,9 +1,12 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:pinpoint_app/api/all_locations.dart';
 import 'package:geolocator/geolocator.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:pinpoint_app/api/all_locations.dart';
 
 class Location extends StatefulWidget {
-  const Location({super.key});
+  const Location({Key? key}) : super(key: key);
 
   @override
   State<Location> createState() => _LocationState();
@@ -11,90 +14,128 @@ class Location extends StatefulWidget {
 
 class _LocationState extends State<Location> {
   late Future<List<User>> futurePositionList;
-  late Position position;
+  late StreamSubscription<Position> locationStreamSubscription;
+  Position? currentPosition;
+  LocationSettings settings = const LocationSettings(
+      accuracy: LocationAccuracy.best, distanceFilter: 0);
+
+  final String apiUrl =
+      'https://pinpoint-api-poc.syand.workers.dev/'; // API URL
 
   @override
   void initState() {
     super.initState();
     _initializeData();
-    _determinePosition();
+    _startContinuousLocationTracking();
   }
 
   Future<void> _initializeData() async {
     futurePositionList = fetchUserList();
   }
 
-  Future<Position> _determinePosition() async {
-    bool serviceEnabled;
-    LocationPermission permission;
+  void _startContinuousLocationTracking() {
+    locationStreamSubscription = Geolocator.getPositionStream(
+      locationSettings: settings,
+    ).listen((Position position) {
+      setState(() {
+        currentPosition = position;
+        String name = "WERKPLZ";
+        postJsonData(name, currentPosition!);
+      });
+    });
+  }
 
-    // Test if location services are enabled.
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      // Location services are not enabled don't continue
-      // accessing the position and request users of the
-      // App to enable the location services.
-      return Future.error('Location services are disabled.');
-    }
+  Future<void> postJsonData(String name, Position pos) async {
+    Map<String, dynamic> jsonData = {
+      "name": name,
+      "lat": pos.latitude,
+      "lon": pos.longitude,
+    };
 
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        // Permissions are denied, next time you could try
-        // requesting permissions again (this is also where
-        // Android's shouldShowRequestPermissionRationale
-        // returned true. According to Android guidelines
-        // your App should show an explanatory UI now.
-        return Future.error('Location permissions are denied');
+    try {
+      final response = await http.put(
+        Uri.parse(apiUrl),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(jsonData),
+      );
+
+      if (response.statusCode == 200) {
+        print('JSON posted successfully');
+      } else {
+        print('Failed to post JSON. Status code: ${response.statusCode}');
       }
+    } catch (e) {
+      print('Error posting JSON: $e');
     }
+  }
 
-    if (permission == LocationPermission.deniedForever) {
-      // Permissions are denied forever, handle appropriately.
-      return Future.error(
-          'Location permissions are permanently denied, we cannot request permissions.');
-    }
-
-    // When we reach here, permissions are granted and we can
-    // continue accessing the position of the device.
-    return await Geolocator.getCurrentPosition();
+  @override
+  void dispose() {
+    // Cancel the location stream subscription when the widget is disposed
+    locationStreamSubscription.cancel();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: FutureBuilder<List>(
-          future: futurePositionList,
-          builder: (context, snapshot) {
-            if (snapshot.hasData &&
-                snapshot.connectionState == ConnectionState.done) {
-              List? positions = snapshot.data;
-              return ListView.builder(
-                itemCount: positions?.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(
-                      "${positions![index].name}: ",
-                      style: const TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(positions[index].lat.toString()),
-                        Text(positions[index].lon.toString()),
-                        Text("Position: ${_determinePosition.toString()}"),
-                      ],
-                    ),
-                  );
+      body: Column(
+        children: [
+          Container(
+            alignment: Alignment.centerLeft,
+            margin: const EdgeInsets.all(15),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "Your location: ",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                if (currentPosition != null)
+                  Text(currentPosition!.latitude.toString()),
+                if (currentPosition != null)
+                  Text(currentPosition!.longitude.toString()),
+              ],
+            ),
+          ),
+          Expanded(
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: FutureBuilder<List>(
+                future: futurePositionList,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData &&
+                      snapshot.connectionState == ConnectionState.done) {
+                    List? users = snapshot.data;
+                    return ListView.builder(
+                      itemCount: users?.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text(
+                            "${users![index].name}: ",
+                            style: const TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          subtitle: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(users[index].lat.toString()),
+                              Text(users[index].lon.toString()),
+                            ],
+                          ),
+                        );
+                      },
+                    );
+                  } else {
+                    return const CircularProgressIndicator();
+                  }
                 },
-              );
-            } else {
-              return const CircularProgressIndicator();
-            }
-          },
-        ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
