6c7ad0612cdfb1399282340db06512147629e607
Sens van Aert <r0878915@student.thomasmore.be>
2023-12-07 13:39:50 +0100

    Button added on contacts page, when pressed, a code is generated

--- code/pinpoint_app/lib/screens/contacts/contacts.dart
+++ code/pinpoint_app/lib/screens/contacts/contacts.dart
@@ -1,13 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:pinpoint_app/components/button.dart';
+import 'package:uuid/uuid.dart';
 
-class Contacts extends StatelessWidget {
+class Contacts extends StatefulWidget {
   const Contacts({super.key});
 
   @override
+  State<Contacts> createState() => _ContactsState();
+}
+
+class _ContactsState extends State<Contacts> {
+  String? uniqueCode;
+
+  void _generateUniqueCode() {
+    var uuid = const Uuid();
+    uniqueCode =
+        uuid.v4().substring(0, 6); // Use a portion of the UUID as the code
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Contacts")
+    return Scaffold(
+      body: Padding(
+        padding:
+            const EdgeInsets.only(top: 60, bottom: 60, left: 30, right: 30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ButtonWidget(
+                  text: "Generate friend code",
+                  onTap: () {
+                    _generateUniqueCode();
+                    setState(() {});
+                  },
+                ),
+                Text(
+                  uniqueCode ?? "No code generated",
+                  style: const TextStyle(fontWeight: FontWeight.w300),
+                ),
+              ],
+            ),
+            const Text("Contacts"),
+          ],
+        ),
       ),
     );
   }
