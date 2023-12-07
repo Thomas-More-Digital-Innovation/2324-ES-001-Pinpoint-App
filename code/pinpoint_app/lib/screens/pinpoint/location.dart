3c0ed579e2fd11903af6ff5db57205af47e60b09
Sens van Aert <r0878915@student.thomasmore.be>
2023-12-07 15:54:46 +0100

    Added function to check permissions

--- code/pinpoint_app/lib/screens/pinpoint/location.dart
+++ code/pinpoint_app/lib/screens/pinpoint/location.dart
@@ -13,8 +13,6 @@ class Location extends StatefulWidget {
 }
 
 class _LocationState extends State<Location> {
-  late bool serviceEnabled;
-  late LocationPermission permission;
   late Future<List<User>> futureUserList;
   late Timer _timer;
   late StreamSubscription<Position> locationStreamSubscription;
@@ -37,17 +35,56 @@ class _LocationState extends State<Location> {
     futureUserList = fetchUserList();
   }
 
-  void _startContinuousLocationTracking() {
+  Future<bool> _askPermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return true;
+  }
+
+  void _startContinuousLocationTracking() async {
     try {
-      locationStreamSubscription = Geolocator.getPositionStream(
-        locationSettings: settings,
-      ).listen((Position position) {
-        setState(() {
-          currentPosition = position;
-          String name = "Jane Doe";
-          postJsonData(name, currentPosition!);
+      if (await _askPermission()) {
+        locationStreamSubscription = Geolocator.getPositionStream(
+          locationSettings: settings,
+        ).listen((Position position) {
+          setState(() {
+            currentPosition = position;
+            String name = "Jane Doe";
+            postJsonData(name, currentPosition!);
+          });
         });
-      });
+      }
     } catch (e) {
       print("There was an excetion in _startContinuousLocationTracking()");
       print(e);
