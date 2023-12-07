dfc35fb666f80f6514948f8243148050a0c391b1
hubevl <r0801228@student.thomasmore.be>
2023-12-07 15:22:28 +0100

    Lay-out custom_map_add

--- code/pinpoint_app/lib/screens/pinpoint/custom_map_add.dart
+++ code/pinpoint_app/lib/screens/pinpoint/custom_map_add.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pinpoint_app/models/map_entry.dart';
 import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 class MapEntryForm extends StatefulWidget {
   final Function(MapEntry) onMapEntryCreated;
@@ -16,7 +17,7 @@ class _MapEntryFormState extends State<MapEntryForm> {
   final TextEditingController _topLeftController = TextEditingController();
   final TextEditingController _bottomRightController = TextEditingController();
 
-  late String _imagePath;
+  late String _imagePath = "";
 
   // Function to handle image selection
   Future<void> _pickImage() async {
@@ -32,50 +33,92 @@ class _MapEntryFormState extends State<MapEntryForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            ElevatedButton(
-              onPressed: _pickImage,
-              child: Text('Select Image'),
+            Expanded(
+              child: Stack(children: [
+                _imagePath != ""
+                    ? Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: FileImage(File(_imagePath)),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      )
+                    : Container(
+                        decoration: BoxDecoration(color: Colors.grey[900]),
+                        child: Center(
+                            child: Icon(Icons.image,
+                                size: 350, color: Colors.red)),
+                      ),
+                Center(
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor:
+                          Colors.white, // Change button background color here
+                    ),
+                    onPressed: _pickImage,
+                    child: Text(
+                      'Select Image',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                ),
+              ]),
             ),
-            TextFormField(
-              controller: _titleController,
-              decoration: InputDecoration(labelText: 'Title'),
-            ),
-            TextFormField(
-              controller: _topLeftController,
-              decoration: InputDecoration(labelText: 'Top Left Coordinate'),
-              keyboardType: TextInputType.number,
-            ),
-            TextFormField(
-              controller: _bottomRightController,
-              decoration: InputDecoration(labelText: 'Bottom Right'),
-              keyboardType: TextInputType.number,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                final String title = _titleController.text;
-                final double cotl = double.parse(_topLeftController.text);
-                final double cobr =
-                    double.parse(_bottomRightController.text);
-
-                final newMapEntry = MapEntry(
-                  imagePath: _imagePath,
-                  title: title,
-                  cotl: cotl,
-                  cobr: cobr,
-                );
-
-                widget.onMapEntryCreated(newMapEntry);
-                Navigator.pop(context);
-              },
-              child: Text('Add Map Entry'),
-            ),
+            Expanded(
+                child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: _titleController,
+                    decoration: InputDecoration(labelText: 'Title'),
+                  ),
+                  TextFormField(
+                    controller: _topLeftController,
+                    decoration:
+                        InputDecoration(labelText: 'Top Left Coordinate'),
+                    keyboardType: TextInputType.number,
+                  ),
+                  TextFormField(
+                    controller: _bottomRightController,
+                    decoration: InputDecoration(labelText: 'Bottom Right Coordinate'),
+                    keyboardType: TextInputType.number,
+                  ),
+                ],
+              ),
+            )),
           ],
         ),
+        floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              final String title = _titleController.text;
+              final double cotl = double.parse(_topLeftController.text);
+              final double cobr = double.parse(_bottomRightController.text);
+
+              final newMapEntry = MapEntry(
+                imagePath: _imagePath,
+                title: title,
+                cotl: cotl,
+                cobr: cobr,
+              );
+
+              widget.onMapEntryCreated(newMapEntry);
+              Navigator.pop(context);
+            },
+            backgroundColor: const Color.fromRGBO(30, 30, 30, 1.0),
+            child: Icon(
+              Icons.check,
+              color: const Color.fromRGBO(161, 255, 182, 100),
+              size: 35,
+            )),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       ),
     );
   }
