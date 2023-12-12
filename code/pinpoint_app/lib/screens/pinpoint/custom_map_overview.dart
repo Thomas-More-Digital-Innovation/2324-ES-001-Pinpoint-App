dc347c95b2e0cdd59334a441882de28b56118ff3
hubevl <r0801228@student.thomasmore.be>
2023-12-12 12:53:12 +0100

    Rework adding map & merging models into 1
    - Reworked custom_map_overview to use the floorplan model instead of the map_entry model
    
    - Reworked custom_map_add to use the floorplan model instead of the map_entry model
    
    - Deleted the map_entry model
    
    - Added functionality of adding a camera image in the CustomMapAdd
    
    - Added functionality of deleting the floorplans in the CustomMapOverView

--- code/pinpoint_app/lib/screens/pinpoint/custom_map_overview.dart
+++ code/pinpoint_app/lib/screens/pinpoint/custom_map_overview.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:pinpoint_app/screens/pinpoint/custom_map_add.dart';
-import 'package:pinpoint_app/models/map_entry.dart';
+import 'package:pinpoint_app/models/floorplan.dart'; // Import your Floorplan model
 
 class CustomMapOverview extends StatefulWidget {
   @override
@@ -10,11 +10,11 @@ class CustomMapOverview extends StatefulWidget {
 }
 
 class _CustomMapOverviewState extends State<CustomMapOverview> {
-  List<MapEntry> _mapEntries = [];
+  List<Floorplan> _floorplans = [];
 
-  void _addMapEntry(MapEntry mapEntry) {
+  void _addFloorplan(Floorplan floorplan) {
     setState(() {
-      _mapEntries.add(mapEntry);
+      _floorplans.add(floorplan);
     });
   }
 
@@ -29,37 +29,46 @@ class _CustomMapOverviewState extends State<CustomMapOverview> {
               width: double.infinity,
               height: 60,
               decoration: const BoxDecoration(
-                  color: Color.fromRGBO(30, 30, 30, 1.0),
-                  borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(15),
-                      bottomRight: Radius.circular(15))),
+                color: Color.fromRGBO(30, 30, 30, 1.0),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(15),
+                  bottomRight: Radius.circular(15),
+                ),
+              ),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  const Text("Map Overview", style: TextStyle(color: Colors.white)),
+                  const Text("Map Overview",
+                      style: TextStyle(color: Colors.white)),
                   Row(
                     children: [
                       IconButton(
-                          onPressed: () => {},
-                          icon: const Icon(
-                            Icons.delete,
-                            color: Color.fromRGBO(255, 70, 70, 1),
-                          )),
+                        onPressed: () {
+                          setState(() {
+                            _floorplans.clear();
+                          });
+                        },
+                        icon: const Icon(
+                          Icons.delete,
+                          color: Color.fromRGBO(255, 70, 70, 1),
+                        ),
+                      ),
                       IconButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => CustomMapAdd(
-                                  onMapEntryCreated: _addMapEntry,
-                                ),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => CustomMapAdd(
+                                onFloorplanCreated: _addFloorplan,
                               ),
-                            );
-                          },
-                          icon: const Icon(
-                            Icons.add,
-                            color: Color.fromRGBO(161, 255, 182, 100),
-                          )),
+                            ),
+                          );
+                        },
+                        icon: const Icon(
+                          Icons.add,
+                          color: Color.fromRGBO(161, 255, 182, 100),
+                        ),
+                      ),
                     ],
                   )
                 ],
@@ -67,60 +76,70 @@ class _CustomMapOverviewState extends State<CustomMapOverview> {
             ),
             Expanded(
               child: ListView.builder(
-                itemCount: _mapEntries.length,
+                itemCount: _floorplans.length,
                 itemBuilder: (BuildContext context, int index) {
-                  final entry = _mapEntries[index];
+                  final floorplan = _floorplans[index];
                   return Container(
-                      margin: const EdgeInsets.only(left: 10, right: 10, top: 10),
-                      width: double.infinity,
-                      height: 150,
-                      decoration: const BoxDecoration(
-                          color: Color.fromARGB(255, 209, 209, 209),
-                          borderRadius: BorderRadius.all(Radius.circular(15))),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            ClipRRect(
-                                borderRadius:
-                                    const BorderRadius.all(Radius.circular(10)),
-                                child: Image.file(
-                                  File(entry.imagePath),
-                                  fit: BoxFit.cover,
-                                  width: 150,
-                                  height: 150,
-                                )),
-                            Expanded(
-                                child: Column(
+                    margin: const EdgeInsets.only(left: 10, right: 10, top: 10),
+                    width: double.infinity,
+                    height: 150,
+                    decoration: const BoxDecoration(
+                      color: Color.fromARGB(255, 209, 209, 209),
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          ClipRRect(
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(10)),
+                            child: Image.file(
+                              File(floorplan.image),
+                              fit: BoxFit.cover,
+                              width: 150,
+                              height: 150,
+                            ),
+                          ),
+                          Expanded(
+                            child: Column(
                               mainAxisAlignment: MainAxisAlignment.start,
                               children: [
                                 Expanded(
-                                    child: Text(entry.title,
-                                        style: const TextStyle(
-                                            fontSize: 16,
-                                            fontWeight: FontWeight.w700))),
+                                  child: Text(
+                                    floorplan.name,
+                                    style: const TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w700),
+                                  ),
+                                ),
                                 Expanded(
                                   child: Container(
                                     padding: const EdgeInsets.all(4),
                                     width: double.infinity,
                                     child: Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceEvenly,
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          Text("CO-TL: ${entry.cotl}"),
-                                          Text("CO-BR: ${entry.cobr}")
-                                        ]),
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceEvenly,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                            "Top Left: ${floorplan.topLeft['lat']}, ${floorplan.topLeft['lon']}"),
+                                        Text(
+                                            "Bottom Right: ${floorplan.bottomRight['lat']}, ${floorplan.bottomRight['lon']}"),
+                                      ],
+                                    ),
                                   ),
                                 )
                               ],
-                            )),
-                          ],
-                        ),
-                      ));
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
                 },
               ),
             ),
