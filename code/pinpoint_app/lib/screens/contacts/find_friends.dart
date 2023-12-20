f15d207d97368da4d523c4fb71a28dc9fa676fd7
Sens van Aert <r0878915@student.thomasmore.be>
2023-12-20 15:07:46 +0100

    Adde buttons that redirects to find friends page

---
+++ code/pinpoint_app/lib/screens/contacts/find_friends.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FiendsFriends extends StatelessWidget {
+  const FiendsFriends({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: Text("Find My Friends")));
+  }
+}
