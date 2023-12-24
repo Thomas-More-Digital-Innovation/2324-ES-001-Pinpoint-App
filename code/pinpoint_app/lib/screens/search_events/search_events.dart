2be907ddd2fddc933c8a1fbdee7d76ea9957a768
Sens van Aert <r0878915@student.thomasmore.be>
2023-12-24 16:18:52 +0100

    A lot of errors because this branched of main but most of the models and pages needed are in another branch 'create events'

--- code/pinpoint_app/lib/screens/search_events/search_events.dart
+++ code/pinpoint_app/lib/screens/search_events/search_events.dart
@@ -1,97 +1,124 @@
 import 'package:flutter/material.dart';
-import 'package:pinpoint_app/globals.dart';
-import 'package:pinpoint_app/models/floorplan.dart';
-import 'package:pinpoint_app/screens/pinpoint/custom_map.dart';
-import 'package:pinpoint_app/api/floorplan_calls.dart';
+import 'package:pinpoint_app/api/event_controller.dart';
+import 'package:pinpoint_app/globals.dart' as globals;
+import 'package:pinpoint_app/models/event.dart';
+import 'package:pinpoint_app/screens/search_events/event_details.dart';
+import 'package:pinpoint_app/screens/search_events/event_overview.dart';
 
 class SearchEvents extends StatefulWidget {
-  const SearchEvents({super.key});
+  const SearchEvents({Key? key}) : super(key: key);
 
   @override
   State<SearchEvents> createState() => _SearchEventsState();
 }
 
 class _SearchEventsState extends State<SearchEvents> {
-  late Future<List<Floorplan>> _futureFloorplans;
+  late Future<List<Event>> _futureEvents;
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    _getFloorplans();
+    _getEvents();
   }
 
-  Future<List<Floorplan>> _getFloorplans() async {
-    _futureFloorplans = fetchFloorplanList();
-    return _futureFloorplans;
+  Future<List<Event>> _getEvents() async {
+    _futureEvents = fetchEventList();
+    return _futureEvents;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: FutureBuilder(
-          future: _futureFloorplans,
-          builder: (context, snapshot) {
-            if (snapshot.hasData &&
-                snapshot.connectionState == ConnectionState.done) {
-              return ListView(
-                  children: snapshot.data!.asMap().entries.map((floorplan) {
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => CustomMap(
-                            centerLat: floorplan.value.centerLat,
-                            centerLon: floorplan.value.centerLon),
-                      ),
-                    );
-                  },
-                  child: Container(
-                      padding: const EdgeInsets.only(
-                          top: 8.0, left: 8.0, right: 8.0),
-                      margin: const EdgeInsets.only(bottom: 8.0),
-                      width: double.infinity,
-                      height: 200,
-                      decoration: const BoxDecoration(
-                        color: Colors.red,
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                      ),
-                      child: Center(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Container(
-                              width: 100, // Set width and height as needed
-                              height: 100,
-                              decoration: const BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: Colors
-                                    .white, // Set the desired background color
-                              ),
-                              child: ClipOval(
-                                child: Image.network(
-                                  floorplan.value.image ?? noImage,
-                                  fit: BoxFit
-                                      .cover, // Adjust the fit based on your requirement
-                                ),
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            Text(floorplan.value.name)
-                          ],
-                        ),
-                      )),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView(
+          children: [
+            FloatingActionButton(
+              heroTag: "toMapMenu",
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const EventOverview(),
+                  ),
                 );
-              }).toList());
-            } else {
-              return const CircularProgressIndicator();
-            }
-          }),
-    ));
+              },
+              backgroundColor: const Color.fromRGBO(255, 255, 255, 1.0),
+              child: const Text("Add Event"),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            FutureBuilder(
+              future: _futureEvents,
+              builder: (context, snapshot) {
+                if (snapshot.hasData &&
+                    snapshot.connectionState == ConnectionState.done) {
+                  return Column(
+                    children: snapshot.data!.asMap().entries.map((event) {
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => EventDetails(
+                                event: event.value,
+                              ),
+                            ),
+                          );
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.only(
+                            top: 8.0,
+                            left: 8.0,
+                            right: 8.0,
+                          ),
+                          margin: const EdgeInsets.only(bottom: 8.0),
+                          width: double.infinity,
+                          height: 200,
+                          decoration: const BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10),
+                            ),
+                          ),
+                          child: Center(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  width: 100,
+                                  height: 100,
+                                  decoration: const BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: Colors.white,
+                                  ),
+                                  child: ClipOval(
+                                    child: Image.network(
+                                      event.value.image ?? globals.noImage,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Text(event.value.title),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                  );
+                } else {
+                  return const CircularProgressIndicator();
+                }
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
