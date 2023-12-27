5cccf926b8ca93c87d9dfd3483b3dae7c051e51c
Sens van Aert <r0878915@student.thomasmore.be>
2023-12-27 10:32:21 +0100

    styling a little better

--- code/pinpoint_app/lib/screens/search_events/event_details.dart
+++ code/pinpoint_app/lib/screens/search_events/event_details.dart
@@ -9,42 +9,47 @@ class EventDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
-      floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.map),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => CustomMap(
-                  centerLat: event.floorplan?.centerLat,
-                  centerLon: event.floorplan?.centerLon,
+    return SafeArea(
+      child: Scaffold(
+        floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
+        floatingActionButton: Padding(
+          padding: const EdgeInsets.fromLTRB(8, 8, 0, 8),
+          child: FloatingActionButton(
+              child: const Icon(Icons.map),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => CustomMap(
+                      centerLat: event.floorplan?.centerLat,
+                      centerLon: event.floorplan?.centerLon,
+                    ),
+                  ),
+                );
+              }),
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              children: [
+                Text(
+                  event.title,
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold, fontSize: 24),
                 ),
-              ),
-            );
-          }),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(50.0),
-          child: Column(
-            children: [
-              Text(
-                event.title,
-                style:
-                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-              ),
-              Container(
-                  alignment: Alignment.bottomLeft,
-                  child: Text("Date: ${event.startDate} - ${event.endDate}")),
-              Image.network(
-                event.image ?? globals.noImage,
-                fit: BoxFit.cover,
-              ),
-              Text(
-                event.description ?? "Ther is no Description for this event!",
-              ),
-            ],
+                Container(
+                    alignment: Alignment.bottomLeft,
+                    child: Text("Date: ${event.startDate} - ${event.endDate}")),
+                Image.network(
+                  event.image ?? globals.noImage,
+                  fit: BoxFit.cover,
+                ),
+                Text(
+                  event.description ?? "Ther is no Description for this event!",
+                ),
+              ],
+            ),
           ),
         ),
       ),
